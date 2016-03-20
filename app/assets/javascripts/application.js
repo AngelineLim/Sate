195fc9a26860d10a07cce49a100770196a77fab2
Angeline <angeline07561@gmail.com>
2016-03-20 13:35:29 +1100

    Fix UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,10 @@
 //= require googlemap
 //= require_tree .
 
-// Loads all Semantic javascripts
-//= require semantic-ui
-//= require semantic-ui/modal
-//= require semantic-ui/dropdownlayout
+Loads all Semantic javascripts
+= require semantic-ui
+= require semantic-ui/modal
+= require semantic-ui/dropdownlayout
 
 //= require_self
 
